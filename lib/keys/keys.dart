afc0ca29dbf17fca11c0cab6537af4f286b0af99
drago008 <prashantmalviya78@gmail.com>
2026-09-13 19:04:20 +0530

    Understanding & Using Keys

--- lib/keys/keys.dart
+++ lib/keys/keys.dart
@@ -71,6 +71,7 @@ class _KeysState extends State<Keys> {
               // for (final todo in _orderedTodos) TodoItem(todo.text, todo.priority),
               for (final todo in _orderedTodos)
                 CheckableTodoItem(
+                  key: ValueKey(todo.text), // ValueKey()
                   todo.text,
                   todo.priority,
                 ),
